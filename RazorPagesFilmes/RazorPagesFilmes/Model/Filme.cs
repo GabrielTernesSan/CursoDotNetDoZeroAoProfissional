943275facd4842c75520294d2af1e1deb04cfd0f
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-10-31 14:45:03 -0300

    Executando projeto

--- RazorPagesFilmes/RazorPagesFilmes/Model/Filme.cs
+++ RazorPagesFilmes/RazorPagesFilmes/Model/Filme.cs
@@ -5,10 +5,10 @@ namespace RazorPagesFilmes.Model
     public class Filme
     {
         public int Id { get; set; }
-        public string Titulo { get; set; } = null!;
+        public string Titulo { get; set; } = string.Empty;
         [DataType(DataType.Date)]
         public DateTimeOffset DataLancamento { get; set; }
-        public string Genero { get; set; } = null!;
+        public string Genero { get; set; } = string.Empty;
         public decimal Preco { get; set; }
     }
 }
--- RazorPagesFilmes/RazorPagesFilmes/Pages/Filmes/Index.cshtml.cs
+++ RazorPagesFilmes/RazorPagesFilmes/Pages/Filmes/Index.cshtml.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using RazorPagesFilmes.Data;
 using RazorPagesFilmes.Model;
 
 namespace RazorPagesFilmes.Pages.Filmes
