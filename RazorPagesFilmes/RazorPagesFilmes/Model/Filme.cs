9b6d954e5c4df3854b6d1f980385342c955004f2
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-11-01 16:02:31 -0300

    DataAnnotations e correção de Bugs

--- RazorPagesFilmes/RazorPagesFilmes/Model/Filme.cs
+++ RazorPagesFilmes/RazorPagesFilmes/Model/Filme.cs
@@ -1,16 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesFilmes.Model
 {
     public class Filme
     {
         public int Id { get; set; }
+        
+        [StringLength(100, MinimumLength = 3)]
         public string Titulo { get; set; } = string.Empty;
+        
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTimeOffset DataLancamento { get; set; }
+
+        [RegularExpression("^[A-ZÀ-Ÿ][a-zA-ZÀ-ÿ\\s]*$")]
+        [StringLength(50)]
         public string Genero { get; set; } = string.Empty;
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Preco { get; set; }
-        public int Classificacao { get; set; }
+
+        [Range(0, 5)]
+        public int Classificacao { get; set; } = 0;
     }
 }
