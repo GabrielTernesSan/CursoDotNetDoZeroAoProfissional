a2e1c23363b80b133ec92fbb76d66aa54214383e
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-11-01 15:16:58 -0300

    Adicionando campo novo

---
+++ RazorPagesFilmes/RazorPagesFilmes/Migrations/20241101181005_alterTableFilme.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesFilmes.Migrations
+{
+    /// <inheritdoc />
+    public partial class alterTableFilme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Classificacao",
+                table: "Filme",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Classificacao",
+                table: "Filme");
+        }
+    }
+}
